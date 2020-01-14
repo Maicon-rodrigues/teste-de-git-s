8bf8896881bf92d484949285b2007f6889cd2d3b
Menor-da-Zs <mai2405rodrigues@gmail.com>
2020-01-14 15:48:33 -0300

    primeiro teste

---
+++ Git.java
@@ -0,0 +1,12 @@
+package ProjetosMaicon;
+
+public class Git {
+    public static void main(String[] args) {
+
+        double altura = 1.79;
+        int idade = 17;
+        String nome = "maicon";
+        System.out.println("seu nome é " + nome + " e tem " + idade + " anos, e sua altura é de " + altura);
+
+    }
+}
